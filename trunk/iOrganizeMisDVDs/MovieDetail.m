cdd20740d10ee0fbaa1e0877c425cdac55df741a
rivera.ernesto <rivera.ernesto@451aae8c-fbf3-11dd-a295-0b7b0935bf94>
2009-02-16 09:19:49 +0000

    Working search, default image

---
+++ trunk/iOrganizeMisDVDs/MovieDetail.m
@@ -0,0 +1,19 @@
+// 
+//  MovieDetail.m
+//  iOrganizeMisDVDs
+//
+//  Created by 利辺羅 on 09/02/16.
+//  Copyright 2009 Piggy Trashing EmperatorZ. All rights reserved.
+//
+
+#import "MovieDetail.h"
+
+
+@implementation MovieDetail 
+
+@dynamic director;
+@dynamic writer;
+@dynamic casting;
+@dynamic length;
+
+@end
